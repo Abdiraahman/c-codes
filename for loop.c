afb07b73290ab71294f868814138ee7a6aec2e62
Your Name <you@example.com>
2023-10-18 15:50:39 +0300

    did math using c

---
+++ for loop.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i;
+    int j;
+    for(i=0; i<3; i++)
+    {
+        for(j=0; j<3; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+    return 0;
+}
