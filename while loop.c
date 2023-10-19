0e9f5e1b5323aa666963e867c4f26d95cdb4794f
Your Name <you@example.com>
2023-10-19 16:45:50 +0300

    did math using c

--- while loop.c
+++ while loop.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 
 int main()
 {
-    int i=0
-    while(i<3)
+    int i=0;
+    while(i<5)
     {
-        printf("%d\n", i);
+        printf("#");
+        i++;
     }
-    i++;
 }
